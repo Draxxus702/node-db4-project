afd951f242571030fb90f0647c7af126e8e307b7
Draxxus702 <58435416+KennithHowe2@users.noreply.github.com>
2020-03-19 15:46:51 -0700

    migration complete

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+// const recipeRouter = require('../router/recipeRouter.js')
+
+const server = express()
+
+server.use(express.json())
+// server.use('/api/recipes', recipeRouter)
+
+server.get('/', (req,res) =>{
+    res.send('Server is running')
+})
+
+module.exports = server
